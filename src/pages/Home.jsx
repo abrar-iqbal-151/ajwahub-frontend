1cd3a125e096fc32377de1e26bb468ed9c7fc9f2
Abrar Iqbal <abrarking141@gmail.com>
2026-05-14 07:00:14 +0500

    feat(home): Add 3D animated background and optimize layout z-index

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -65,6 +65,18 @@ function Home() {
 
   return (
     <div className="home-page">
+      {/* 3D Background */}
+      <div className="desc-bg-3d">
+        <div className="desc-bg-grid" />
+        <div className="desc-orb desc-orb1" />
+        <div className="desc-orb desc-orb2" />
+        <div className="desc-orb desc-orb3" />
+        <div className="desc-orb desc-orb4" />
+        <div className="desc-bg-lines">
+          {[...Array(6)].map((_,i) => <div key={i} className="desc-bg-line" style={{animationDelay:`${i*0.4}s`}} />)}
+        </div>
+      </div>
+
       <Navbar />
 
       {/* SLIDER */}
