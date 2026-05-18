8f23ce0b561f777a8088ac9f5b2f98552fcfc8bf
Abrar Iqbal <abrarking141@gmail.com>
2026-05-18 23:35:49 +0500

    style: remove inline marginBottom override to give proper space under mobile header

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -223,7 +223,7 @@ function Description() {
 
       {/* FEATURE SECTION */}
       <section className="desc-feature-section">
-        <h3 className="section-title" style={{ gridColumn: '1/-1', textAlign: 'center', marginBottom: '0' }}>Our Premium AjwaHub</h3>
+        <h3 className="section-title" style={{ gridColumn: '1/-1', textAlign: 'center' }}>Our Premium AjwaHub</h3>
         <div className="desc-feature-img-wrap">
           <div className="desc-float-single">
             {feature && feature.images && feature.images.length > 0 ? (
